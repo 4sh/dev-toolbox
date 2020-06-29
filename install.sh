4cd409ca31ca855a5b3cf8010f967badb0623dfa
Xavier Hanin <xavier.hanin@4sh.fr>
2020-06-29 17:02:06 +0200

    fix install

--- install.sh
+++ install.sh
@@ -7,15 +7,17 @@ git clone git@github.com:4sh/dev-toolbox.git
 mv dev-toolbox $HOME/.4dev-toolbox
 
 if [ -f "$HOME/.zshrc" ]; then
-  echo '\nsource $HOME/.4dev-toolbox/toolsrc' >> $HOME/.zshrc
-  echo "installed in .zshrc - please open a new shell to get functions"
+  target=.zshrc
 elif [ -f "$HOME/.bashrc" ]; then
-  echo '\nsource $HOME/.4dev-toolbox/toolsrc' >> $HOME/.bashrc
-  echo "installed in .bashrc - please open a new shell to get functions"
+  target=.bashrc  
 elif [ -f "$HOME/.bash_profile" ]; then
-  echo '\nsource $HOME/.4dev-toolbox/toolsrc' >> $HOME/.bash_profile
-  echo "installed in .bash_profile - please open a new shell to get functions"
+  target=.bash_profile
 else
   echo "source env file not found - please add this to your env init script"
   echo '    source $HOME/.4dev-toolbox/toolsrc'
+  exit 1
 fi
+
+echo '' >> $HOME/$target
+echo 'source $HOME/.4dev-toolbox/toolsrc' >> $HOME/$target
+echo "installed in $target - please open a new shell to get functions"
