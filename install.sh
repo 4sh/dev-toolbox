b099f59105d868fba43d682bf7ad9c3556787ab8
Xavier Hanin <xavier.hanin@4sh.fr>
2020-07-01 08:28:41 +0200

    improve install script
        check if already cloned
        error message if git clone fails
        check if already sourced
        add to all rc files

--- install.sh
+++ install.sh
@@ -2,25 +2,42 @@
 
 set -eu -o pipefail
 
-git clone git@github.com:4sh/dev-toolbox.git 
-
-mv dev-toolbox $HOME/.4dev-toolbox
-
-if [ -f "$HOME/.zshrc" ]; then
-  target=.zshrc
-elif [ -f "$HOME/.bashrc" ]; then
-  target=.bashrc  
-elif [ -f "$HOME/.bash_profile" ]; then
-  target=.bash_profile
+if [ -f $HOME/.4dev-toolbox/toolsrc ]; then
+  echo "4dev-toolbox already present - updating..."
+  cd  $HOME/.4dev-toolbox
+  git pull --rebase || (
+    echo "4dev-toolbox already installed, but couldn't update it"
+  )
 else
-  echo "source env file not found - please add this to your env init script"
+  git clone git@github.com:4sh/dev-toolbox.git || (
+    echo "
+  FAILED - failed to clone github repository
+  Please check you have a github ssh key properly setup"
+    exit 1
+  )
+
+  mv dev-toolbox $HOME/.4dev-toolbox
+  echo "4dev-toolbox installed in ~/.4dev-toolbox"
+fi
+
+source $HOME/.4dev-toolbox/toolsrc
+
+found=0
+for rc in .zshrc .bashrc .bash_profile ; do
+    if [ -f "$HOME/$rc" ]; then
+      if [[ `grep ".4dev-toolbox" $HOME/$rc` ]]; then
+        found=1
+        continue
+      fi
+      echo '' >> $HOME/$rc
+      echo 'source $HOME/.4dev-toolbox/toolsrc' >> $HOME/$rc
+      echo "4dev-toolbox added to $rc"
+    fi
+done
+
+if [ $found == 0 ]; then
+  echo "source env file not found - please add this to your env init script:"
   echo '    source $HOME/.4dev-toolbox/toolsrc'
   exit 1
 fi
 
-echo '' >> $HOME/$target
-echo 'source $HOME/.4dev-toolbox/toolsrc' >> $HOME/$target
-
-source $HOME/.4dev-toolbox/toolsrc
-
-echo "installed in $target and ready to use"
